8c92fdd39ad8126dc73ec3ef772ca04803e1e278
RAFINHAMOREIRA08 <rafah.bacha@gmail.com>
2026-03-10 10:02:57 -0300

    Término da resolução do Exercício 3

--- src/exercicio03/BilheteUnico.java
+++ src/exercicio03/BilheteUnico.java
@@ -9,27 +9,29 @@ public class BilheteUnico {
     String tipoTarifa;
     final static double TARIFABASE = 5.40;
 
-    public BilheteUnico (String usuario, String tipoTarifa) {
+    public BilheteUnico(String usuario, String tipoTarifa) {
         Random random = new Random();
         this.usuario = usuario;
         this.tipoTarifa = tipoTarifa;
-        this.numero = 1000 + random.nextInt(9999);
+        this.numero = random.nextInt(1000, 9999);
     }
 
-    public void passarNaCatraca() {
+    public boolean passarNaCatraca() {
         double valor = TARIFABASE;
-        if(tipoTarifa.equalsIgnoreCase("Estudante") ||
-                tipoTarifa.equalsIgnoreCase("Professor")) {
+        if(tipoTarifa.equalsIgnoreCase("estudante") ||
+                tipoTarifa.equalsIgnoreCase("professor")) {
             valor = valor / 2;
         }
 
-        if (saldo < valor) {
-            return;
+        if(saldo < valor) {
+            return false;
         }
 
         saldo = saldo - valor;
+        return true;
     }
 
-
+    public void carregar(double valor) {
+        this.saldo += valor;
+    }
 }
-
